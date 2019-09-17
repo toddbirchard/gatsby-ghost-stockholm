a9e923ef9cef7bfd81d992d4287046064c6bd783
Todd Birchard <toddbirchard@gmail.com>
2019-09-16 22:04:52 -0400

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import { Layout } from '../components/common'
 
+import '../styles/pages/404.less'
+
 const NotFoundPage = () => (
-    <Layout>
-        <div className="container">
-            <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
-                <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
-                </section>
-            </article>
+    <Layout template="bg-purple error-template">
+        <div className="stars">
+            <div className="central-body">
+                <h1 className="error-title">404</h1>
+                <Link to="/" className="home-button" target="_blank">GO BACK HOME</Link>
+            </div>
+            <div className="objects">
+                <img className="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px" />
+                <div className="earth-moon">
+                    <img className="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" />
+                    <img className="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" />
+                </div>
+                <div className="box_astronaut">
+                    <img className="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" />
+                </div>
+            </div>
+            <div className="glowing_stars">
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+                <div className="star"></div>
+            </div>
         </div>
     </Layout>
 )
