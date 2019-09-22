374e0fc2db786e03dc283cb6640856adbd67cab0
Todd Birchard <toddbirchard@gmail.com>
2019-09-22 18:18:45 -0400

    links and RSS url issue

--- src/components/sidebar/SocialWidget.js
+++ src/components/sidebar/SocialWidget.js
@@ -7,6 +7,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRss, faTag } from '@fortawesome/pro-regular-svg-icons'
 
+import config from '../../utils/siteConfig'
+
 /**
 * Social widget
 */
@@ -17,11 +19,11 @@ const SocialWidget = ({ twitterUrl, facebookUrl }) => (
       <>
         <div className="widget social">
             <a href={ twitterUrl } className="twitter" key="twitter-sidebar"><FontAwesomeIcon icon={[`fab`, `twitter`]} size="sm" /></a>
-            <a href="https://angel.co/company/hackers-and-slackers/" className="angellist" key="angellist-sidebar"><FontAwesomeIcon icon={[`fab`, `angellist`]} size="sm" /></a>
-            <a href="https://www.linkedin.com/in/hackersandslackers/" className="linkedin" key="linkedin-sidebar"><FontAwesomeIcon icon={[`fab`, `linkedin`]} size="sm" /></a>
-            <a href="https://github.com/hackersandslackers" className="github" key="github-sidebar"><FontAwesomeIcon icon={[`fab`, `github`]} size="sm" /></a>
+            <a href={ config.social.angellist } className="angellist" key="angellist-sidebar"><FontAwesomeIcon icon={[`fab`, `angellist`]} size="sm" /></a>
+            <a href={ config.social.linkedin } className="linkedin" key="linkedin-sidebar"><FontAwesomeIcon icon={[`fab`, `linkedin`]} size="sm" /></a>
+            <a href={ config.social.github } className="github" key="github-sidebar"><FontAwesomeIcon icon={[`fab`, `github`]} size="sm" /></a>
             <a href={ facebookUrl } className="facebook" key="facebook-sidebar"><FontAwesomeIcon icon={[`fab`, `facebook`]} size="sm" /></a>
-            <Link to="/rss/" className="rss" key="rss-sidebar"><FontAwesomeIcon icon={[`far`, `rss`]} size="sm" /></Link>
+            <Link to="/rss" className="rss" key="rss-sidebar"><FontAwesomeIcon icon={[`far`, `rss`]} size="sm" /></Link>
         </div>
     </>
 )
