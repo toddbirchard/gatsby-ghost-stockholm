50184ed467898d2bfa1291994e4ca20e96d9e5d2
Todd Birchard <toddbirchard@gmail.com>
2019-10-14 17:22:08 -0400

    updated packages

--- src/components/authors/index.js
+++ src/components/authors/index.js
@@ -1,4 +1,3 @@
 export { default as AuthorList } from './AuthorList'
 export { default as AuthorCard } from './AuthorCard'
-export { default as AuthorGithub } from './AuthorGithub'
 export { default as AuthorSidebar } from './AuthorSidebar'
