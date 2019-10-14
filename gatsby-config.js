0db2d190a8502addb4677e630834bfc739b4f078
Todd Birchard <toddbirchard@gmail.com>
2019-10-14 17:16:33 -0400

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -258,7 +258,7 @@ module.exports = {
                   include_rts: false,
                   exclude_replies: true,
                   tweet_mode: "extended",
-                  count: 6,
+                  count: 8,
                 },
               },
             },
