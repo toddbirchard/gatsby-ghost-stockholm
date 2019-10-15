c4a60c45dbfe176f5a324a3f6de8bc967bf1293f
Todd <todd.birchard@fanduel.com>
2019-10-15 01:46:25 -0400

    code snippet styles etc

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faRss, faTag } from '@fortawesome/pro-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -25,22 +24,15 @@ const Footer = ({ navigation, site, data }) => {
         {/* The footer at the very bottom of the screen */}
         <footer className="site-footer">
             <div className="footer-wrapper">
-                <div className="widget about">
+                {/*<div className="widget about">
                     <Link to="/" alt="/">
-                        {site.logo ? <img className="logo" src={site.logo} alt={site.title} /> : <Img className="logo" fixed={site.logo} alt={site.title} loading="lazy" /> }
+                        {site.logo ? <img className="logo" src="/light-logo-small-2.png" alt={site.title} /> : <Img className="logo" fixed={site.logo} alt={site.title} loading="lazy" /> }
                     </Link>
                     <p className="description">{config.description}</p>
                     <div className="social-btns">
-                        <a href={ twitterUrl } className="twitter" key="twitter-footer"><FontAwesomeIcon icon={[`fab`, `twitter`]} size="sm" /></a>
-                        <a href={ facebookUrl } className="facebook" key="facebook-footer"><FontAwesomeIcon icon={[`fab`, `facebook`]} size="sm" /></a>
-                        <a href={ config.social.angellist } className="angellist-footer" key="angellist"><FontAwesomeIcon icon={[`fab`, `angellist`]} size="sm" /></a>
-                        {/*<a href={ config.social.linkedin } className="linkedin-footer" key="linkedin"><FontAwesomeIcon icon={[`fab`, `linkedin`]} size="sm" /></a>*/}
-                        <a href={ config.social.github } className="github" key="github-footer"><FontAwesomeIcon icon={[`fab`, `github`]} size="sm" /></a>
-                        {/*<a href={ config.social.tumblr } className="tumblr" key="tumblr-footer"><FontAwesomeIcon icon={[`fab`, `tumblr`]} size="sm" /></a>*/}
-                        <Link to="/rss/" className="rss" key="rss"><FontAwesomeIcon icon={[`far`, `rss`]} size="sm" /></Link>
-                    </div>
 
-                </div>
+                    </div>
+                </div>*/}
                 <div className="widget links">
                     <h5 className="footer-widget-title">Links</h5>
                     {navigation.map((navItem, i) => {
@@ -63,6 +55,16 @@ const Footer = ({ navigation, site, data }) => {
                     {authorLinks.map(({ node }) => (
                         <Link to={`/author/${ node.slug }`} className="footer-link" key={ node.name } >{ node.name }</Link>
                     ))}
+                </div>
+                <div className="widget social">
+                    <h5 className="footer-widget-title">Social</h5>
+                    <a href={ twitterUrl } className="twitter footer-link" key="twitter-footer">Twitter</a>
+                    <a href={ facebookUrl } className="facebook footer-link" key="facebook-footer">Facebook</a>
+                    <a href={ config.social.angellist } className="angellist-footer footer-link" key="angellist">Angellist</a>
+                    {/*<a href={ config.social.linkedin } className="linkedin-footer" key="linkedin"><FontAwesomeIcon icon={[`fab`, `linkedin`]} size="sm" /></a>*/}
+                    <a href={ config.social.github } className="github footer-link" key="github-footer">Github</a>
+                    {/*<a href={ config.social.tumblr } className="tumblr" key="tumblr-footer"><FontAwesomeIcon icon={[`fab`, `tumblr`]} size="sm" /></a>*/}
+                    <Link to="/rss/" className="rss footer-link" key="rss">RSS</Link>
                 </div>
             </div>
             <div className="copyright">
