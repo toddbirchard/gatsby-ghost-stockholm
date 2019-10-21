2327cc9d5af7a42d2e13bcad3205fb49bc9eb46d
Todd Birchard <toddbirchard@gmail.com>
2019-10-20 21:17:03 -0400

    rss query

--- src/utils/rss/generate-feed.js
+++ src/utils/rss/generate-feed.js
@@ -109,7 +109,7 @@ const generateRSSFeed = function generateRSSFeed(siteConfig) {
                         url
                     }
                 }
-
+              }
         }
   `,
         output: `/rss`,
