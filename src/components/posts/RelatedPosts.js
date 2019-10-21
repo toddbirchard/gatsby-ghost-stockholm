69b1423ba970efcfd7c2ffe9ddab27408a2ee3b7
Todd Birchard <toddbirchard@gmail.com>
2019-10-20 22:46:49 -0400

    added margin between relate post tags

--- src/components/posts/RelatedPosts.js
+++ src/components/posts/RelatedPosts.js
@@ -16,7 +16,7 @@ const RelatedPosts = ({ data }) => {
                       <div className="related-post-info">
                           <h5 className="related-post-title"> { node.title } </h5>
                           <div className="meta-item related-post-tags">
-                              <FontAwesomeIcon icon={[`far`, `tag`]} /><Tags post={node} limit={2} visibility="public" autolink={false} classes="tag" separator=", " />
+                              <FontAwesomeIcon icon={[`far`, `tag`]} /><Tags post={node} limit={2} visibility="public" autolink={false} classes="tag" separator=", " separatorClasses="tag-separator"/>
                           </div>
                       </div>
                   </Link>
