562f92cbc8572ba35ccd1edf6102ca61ff14a123
Todd Birchard <toddbirchard@gmail.com>
2019-10-26 14:48:49 -0400

    fixed fonts

--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -3,20 +3,18 @@ import PropTypes from 'prop-types'
 import config from '../../utils/siteConfig'
 import { Link } from 'gatsby'
 
-
 const NavLinks = ({ navigation }) => (
     <>
       {navigation.map((navItem, i) => {
           if (navItem.url.includes(`rss`) || navItem.url.includes(`sitemap`)) {
-              return <a className="navigation-link" href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>
+              return <a className="navigation-link rss" href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>
           } else if (navItem.url.includes(config.siteUrl)) {
               return <Link className="navigation-link" to={`${navItem.url.split(`/`).pop()}`} key={i} >{navItem.label}</Link>
           } else {
-              return <a className="navigation-link" href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>
+              return <a className="navigation-link donate" href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>
           }
       })}
   </>
 )
-
 
 export default NavLinks
