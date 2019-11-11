25226361ece06095d6faeb2a1d5ac213f5491cdb
Todd Birchard <toddbirchard@gmail.com>
2019-11-11 09:24:37 -0500

    mysql widget fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -257,13 +257,14 @@ module.exports = {
           options: {
             connectionDetails: {
               host: process.env.MYSQL_HOST,
+              port: process.env.MYSQL_PORT,
               user: process.env.MYSQL_USERNAME,
               password: process.env.MYSQL_PASSWORD,
               database: process.env.MYSQL_DATABASE_NAME,
             },
             queries: [
               {
-                statement: `SELECT * FROM analytics`,
+                statement: `SELECT * FROM hackers`,
                 idFieldName: `id`,
                 name: `analytics`
               }
