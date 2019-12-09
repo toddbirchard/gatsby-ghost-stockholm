c9b659d9199a759f3a042915d6a3fcad8eae9567
Todd Birchard <toddbirchard@gmail.com>
2019-12-09 03:05:29 -0500

    hide newsletter posts

--- src/templates/index.js
+++ src/templates/index.js
@@ -52,7 +52,7 @@ export default Index
 export const pageQuery = graphql`
   query GhostPostQuery($limit: Int!, $skip: Int!) {
     allGhostPost(
-        sort: { order: DESC, fields: [published_at]}, filter: {tags: {elemMatch: {slug: {nin: ["roundup", "#newsletter"]}}}},
+        sort: { order: DESC, fields: [published_at]}, filter: {tags: {elemMatch: {slug: {nin: ["roundup", "#newsletter", "hash-newsletter"]}}}},
         limit: $limit,
         skip: $skip
     ) {
