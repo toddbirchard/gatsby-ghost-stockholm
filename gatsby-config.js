b5aed8104f304c41d994b7ef689f046106504c7a
Todd Birchard <toddbirchard@gmail.com>
2019-12-11 02:49:05 -0500

    gotham fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -276,23 +276,6 @@ module.exports = {
         `gatsby-plugin-force-trailing-slashes`,
         `gatsby-plugin-offline`,
         {
-          resolve: `gatsby-plugin-netlify`,
-          options: {
-            headers: {
-              "/static/fonts.css" : [
-                'referer: https://hackersandslackers.com/',
-                'scheme: https',
-                'origin: https://hackersandslackers.com'
-                ]}, // option to add more headers. `Link` headers are transformed by the below criteria
-            allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-            mergeSecurityHeaders: true, // boolean to turn off the default security headers
-            mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-            mergeCachingHeaders: true, // boolean to turn off the default caching headers
-            transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-            generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-          },
-        },
-        {
             resolve: `gatsby-plugin-segment-js`,
             options: {
                 prodKey: process.env.SEGMENT_WRITE_KEY,
@@ -313,6 +296,5 @@ module.exports = {
             },
         },
         `gatsby-plugin-styled-components`,
-        'gatsby-plugin-preload-link-crossorigin',
     ],
 }
