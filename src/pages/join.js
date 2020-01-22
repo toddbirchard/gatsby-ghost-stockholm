d57ba214421ef17e8a9fd4c1c94d101cbcd854a8
Todd <todd.birchard@fanduel.com>
2020-01-22 18:23:53 -0500

    life is meaningless

--- src/pages/join.js
+++ src/pages/join.js
@@ -7,13 +7,8 @@ import { MetaData } from '../components/common/meta'
 
 import '../styles/pages/page.less'
 
-const JoinUs = ({ data, location, pageContext }) => (
+const JoinUs = ({ data, location }) => (
     <>
-        <MetaData
-            data={data}
-            location={location}
-            type="website"
-        />
         <Layout template="page-template join-template" hasSidebar={false}>
             <article className="post-content page-content">
                 <h1>{`Join Us`}</h1>
