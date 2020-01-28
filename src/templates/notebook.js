ed270ea2a2f2f9336c1fb465ae3f645c77f48ac6
Todd Birchard <toddbirchard@gmail.com>
2020-01-28 01:02:31 -0500

    removed netlify font headers

--- src/templates/notebook.js
+++ src/templates/notebook.js
@@ -5,6 +5,7 @@ import { Layout } from '../components/common'
 // import NotebookPreview from "@nteract/notebook-preview"
 
 import '../styles/jupyter/index.less'
+import '../styles/posts/code.less'
 
 /**
 * Single notebook view (/jupyter/:slug)
@@ -16,27 +17,34 @@ import '../styles/jupyter/index.less'
 const JupyterNotebook = ({ data, pageContext }) => {
     const file = data.file
     const notebook = file.childJupyterNotebook
-    const languageName = notebook.metadata ? notebook.metadata.language_info.name : null
-    const languageVersion = notebook.metadata ? notebook.metadata.language_info.version : null
+    const languageName = notebook.metadata
+        ? notebook.metadata.language_info.name
+        : null
+    const languageVersion = notebook.metadata
+        ? notebook.metadata.language_info.version
+        : null
     const githubLink = file.gitRemote.href + file.relativePath
     const githubRepoName = file.gitRemote.full_name
 
-    return (
-        <>
-            <Layout template="jupyter-template">
-                <div className="jupyter-container">
-                    <h1>{pageContext.title}</h1>
-                    <div className="jupyter-meta">
-                        <div className="meta-item jupyter-language">{languageName} {languageVersion}</div>
-                        <div className="meta-item jupyter-origin-url"><a href={githubLink}>{githubRepoName}</a></div>
-                        <div className="meta-item jupyter-date">{file.modifiedTime}</div>
+    return (<>
+        <Layout template="jupyter-template">
+            <div className="jupyter-container">
+                <h1>{pageContext.title}</h1>
+                <div className="jupyter-meta">
+                    <div className="meta-item jupyter-language">{languageName}
+                        {languageVersion}</div>
+                    <div className="meta-item jupyter-origin-url">
+                        <a href={githubLink}>{githubRepoName}</a>
                     </div>
-                    <main className="post-content content-body load-external-scripts" dangerouslySetInnerHTML={{ __html: notebook.html }}></main>
-                    {/*<NotebookPreview notebook={notebook.internal.content} />*/}
+                    <div className="meta-item jupyter-date">{file.modifiedTime}</div>
                 </div>
-            </Layout>
-        </>
-    )
+                <main className="post-content content-body load-external-scripts" dangerouslySetInnerHTML={{
+                    __html: notebook.html,
+                }}></main>
+                {/*<NotebookPreview notebook={notebook.json}/>*/}
+            </div>
+        </Layout>
+    </>)
 }
 
 JupyterNotebook.propTypes = {
@@ -46,36 +54,26 @@ JupyterNotebook.propTypes = {
                 id: PropTypes.string.isRequired,
                 fileAbsolutePath: PropTypes.string.isRequired,
                 html: PropTypes.string.isRequired,
-                internal: PropTypes.shape({
-                    content: PropTypes.string.isRequired,
-                }),
+                json: PropTypes.object,
+                internal: PropTypes.shape({ content: PropTypes.string.isRequired }),
                 metadata: PropTypes.shape({
-                    language_info: PropTypes.shape({
-                        name: PropTypes.string.isRequired,
-                        version: PropTypes.string.isRequired,
-                    }),
+                    language_info: PropTypes.shape({ name: PropTypes.string.isRequired, version: PropTypes.string.isRequired }),
                 }),
             }).isRequired,
             slug: PropTypes.string,
             name: PropTypes.string.isRequired,
             modifiedTime: PropTypes.string,
             relativePath: PropTypes.string,
-            gitRemote: PropTypes.shape({
-                href: PropTypes.string,
-                full_name: PropTypes.string.isRequired,
-            }),
-
+            gitRemote: PropTypes.shape({ href: PropTypes.string, full_name: PropTypes.string.isRequired }),
         }).isRequired,
     }),
-    pageContext: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-    }),
+    pageContext: PropTypes.shape({ title: PropTypes.string.isRequired }),
     location: PropTypes.object.isRequired,
 }
 
 export default JupyterNotebook
 
-export const JupyterNotebookQuery = graphql`
+export const JupyterNotebookQuery = graphql `
   query($id: String!) {
     file(id: {eq: $id}) {
      childJupyterNotebook {
@@ -85,6 +83,14 @@ export const JupyterNotebookQuery = graphql`
        internal {
         content
       }
+      json {
+            nbformat
+            nbformat_minor
+            cells {
+              cell_type
+              execution_count
+            }
+          }
        metadata {
          language_info {
            name
