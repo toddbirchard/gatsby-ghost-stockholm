730d4cda40afbbec94f5900d347cbd2372ab761c
Todd <todd.birchard@fanduel.com>
2020-01-29 19:48:59 -0500

    commento optimization

--- src/components/posts/Commento.js
+++ src/components/posts/Commento.js
@@ -25,7 +25,7 @@ const Commento = ({ id }) => {
         const document = window.document
         // In case our #commento container exists we can add our commento script
         if (document.getElementById(`commento`)) {
-            insertScript(`/js/commento.js`, `commento-script`, document.body)
+            insertScript(`https://cdn.commento.io/js/commento.js`, `commento-script`, document.body)
         }
         // Cleanup; remove the script from the page
         return () => removeScript(`commento-script`, document.body)
