9913a5564e0ba07188a19e9d34e933c5d55e6131
Todd Birchard <toddbirchard@gmail.com>
2020-02-06 09:46:13 -0500

    social href

--- src/components/sidebar/SocialWidget.js
+++ src/components/sidebar/SocialWidget.js
@@ -12,9 +12,9 @@ const SocialWidget = ({ twitterUrl, facebookUrl }) => (
     <>
         <div className="widget social">
             <a href={ twitterUrl } className="twitter" key="twitter-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `twitter`]} size="xs" /></a>
-            <a href={ config.social.medium } className="medium" key="medium-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `medium`]} size="xs" /></a>
+            <a href={ config.social.mediumProfile } className="medium" key="medium-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `medium`]} size="xs" /></a>
             <a href={ config.social.linkedinProfile } className="linkedin" key="linkedin-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `linkedin`]} size="xs" /></a>
-            <a href={ config.social.github } className="github" key="github-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `github`]} size="xs" /></a>
+            <a href={ config.social.githubProfile } className="github" key="github-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `github`]} size="xs" /></a>
             <a href={ facebookUrl } className="facebook" key="facebook-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fab`, `facebook`]} size="xs" /></a>
             <a href={ config.social.feedlyProfile } className="rss" key="rss-sidebar" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={[`fad`, `rss-square`]} size="xs" /></a>
         </div>
