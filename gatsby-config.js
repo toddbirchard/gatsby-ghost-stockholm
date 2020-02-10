cf22012f26dc32ac44d4dff31bc803b4b399051e
Todd Birchard <toddbirchard@gmail.com>
2020-02-09 22:05:29 -0500

    page foot widget width

--- gatsby-config.js
+++ gatsby-config.js
@@ -196,7 +196,7 @@ module.exports = {
                     require(`stylelint`)()],
             },
         },
-        /*{
+        {
             resolve: `gatsby-plugin-purgecss`,
             options: {
                 printRejected: true, // Print removed selectors and processed file names
@@ -204,10 +204,10 @@ module.exports = {
                 // tailwind: true, // Enable tailwindcss support
                 whitelist: [`searchbox`, `search-result`, `tableContainer`], // Don't remove this selector
                 whitelistPatterns: [`*-template$`],
-                //ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+                ignore: [`/ignored.css`, `prismjs/`, `docsearch.js/`], // Ignore files/folders
                 // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
             },
-        },*/
+        },
         /**
          *  Netlify Plugins
          */
