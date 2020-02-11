0ac7f8bb3ebc6e2ac961cd6d83dc3ae8a612754e
Todd Birchard <toddbirchard@gmail.com>
2020-02-10 20:56:37 -0500

    styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -187,18 +187,19 @@ module.exports = {
                 parser: `less`,
                 postCssPlugins: [
                     require(`autoprefixer`)(),
-                    require(`stylelint`)()],
+                    require(`stylelint`)(),
+                    require(`cssnano`)()],
             },
         },
         {
             resolve: `gatsby-plugin-purgecss`,
             options: {
                 printRejected: true, // Print removed selectors and processed file names
-                develop: false, // Enable while using `gatsby develop`
+                develop: true, // Enable while using `gatsby develop`
                 whitelist: [`searchbox`, `search-result`, `tableContainer`], // Don't remove this selector
-                whitelistPatterns: [`*-template$`],
+                whitelistPatterns: [`*-template$`, `.content-body > pre`],
                 ignore: [`/ignored.css`, `prismjs/`, `docsearch.js/`], // Ignore files/folders
-                // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+                // purgeOnly : ['components/', '/main.css'], // Purge only these files/folders
             },
         },
         /**
