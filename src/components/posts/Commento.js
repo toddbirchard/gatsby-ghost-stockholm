57aacad3e3c3b07025aeaf81c94351ca3d2bc455
Todd Birchard <toddbirchard@gmail.com>
2020-02-16 05:21:07 -0500

    commento

--- src/components/posts/Commento.js
+++ src/components/posts/Commento.js
@@ -1,17 +1,13 @@
 import React, { useEffect } from 'react'
 
-const commentoStyles = `/css/commento.css`
-const commentoJS = `/js/commento.js`
-// const commentoJS = config.siteUrl + `/js/commento.js`
-
-// Helper to add scripts to our page
 const insertScript = (src, id, parentElement) => {
     const script = window.document.createElement(`script`)
     script.async = true
     script.src = src
     script.id = id
-    script.setAttribute(`data-css-override`, commentoStyles)
-    script.setAttribute(`data-no-fonts`, `true`)
+    script.setAttribute(`data-css-override`, `/css/commento.css`)
+    script.setAttribute(`data-no-fonts`, true)
+    script.setAttribute(`data-auto-init`, true)
     parentElement.appendChild(script)
     return script
 }
@@ -22,22 +18,21 @@ const removeScript = (id, parentElement) => {
         parentElement.removeChild(script)
     }
 }
-
+// The actual component
 const Commento = ({ id }) => {
     useEffect(() => {
-        // If there's no window there's nothing to do for us
+    // If there's no window there's nothing to do for us
         if (!window) {
             return
         }
         const document = window.document
         // In case our #commento container exists we can add our commento script
         if (document.getElementById(`commento`)) {
-            insertScript(commentoJS, `commento-script`, document.body)
+            insertScript(`https://cdn.commento.io/js/commento.js`, `commento-script`, document.body)
         }
         // Cleanup; remove the script from the page
         return () => removeScript(`commento-script`, document.body)
     }, [id])
     return <div id={`commento`} />
 }
-
 export default Commento
