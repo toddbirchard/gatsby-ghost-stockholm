4cbff41144000f0276b8335db69dc8b63544ec58
Todd <todd.birchard@fanduel.com>
2020-02-20 17:09:27 -0500

    fix sidebar styles

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -5,7 +5,7 @@ import { Tags } from '@tryghost/helpers-gatsby'
 import { readingTime as readingTimeHelper } from '@tryghost/helpers'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import '../styles/post-card.less'
+import '../../styles/post-card.less'
 
 const PostCard = ({ post }) => {
     const url = post.slug.includes(`lynx`) ? `/roundup/${post.slug}/` : `/${post.slug}/`
