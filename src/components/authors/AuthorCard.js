1e51d0aed6fde7fb0bf49d940eca1cca9a18d53b
Todd Birchard <toddbirchard@gmail.com>
2020-04-26 22:21:39 -0400

    Broken npm package.

--- src/components/authors/AuthorCard.js
+++ src/components/authors/AuthorCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import { FaPencilAlt, FaTwitter, FaFacebook, FaHome, FaGlobe } from 'react-icons/fa'
+import { FaPencilAlt, FaTwitter, FaHome, FaGlobe } from 'react-icons/fa'
 
 import '../../styles/author-card.less'
 
