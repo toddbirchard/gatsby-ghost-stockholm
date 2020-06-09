394f031bfb13628214ebe777789d14bd72e6f53b
Todd Birchard <toddbirchard@gmail.com>
2020-06-08 22:47:25 -0400

    Break series into sections.

---
+++ src/pages/modules/index.js
@@ -0,0 +1,1 @@
+export { default as CoursePreview } from './CoursePreview'
--- src/pages/seriesarchive.js
+++ src/pages/seriesarchive.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
+import { CoursePreview } from './modules'
 
 import '../styles/pages/seriesarchive.less'
 import '../styles/pages/page.less'
@@ -13,8 +14,12 @@ import '../styles/pages/page.less'
 * Lists all multi-part post series.
 *
 */
+
 const SeriesArchive = ({ data, location, pageContext }) => {
-  const tags = data.allGhostTag.edges
+  const dataScienceCourses = data.datascience.edges
+  const softwareCourses = data.software.edges
+  const analysisCourses = data.analysis.edges
+  const cloudCourses = data.cloud.edges
   const title = pageContext.title
   const description = pageContext.description
   const metaDescription = pageContext.metaDescription
@@ -35,17 +40,31 @@ const SeriesArchive = ({ data, location, pageContext }) => {
           </div>
           <p>{description}</p>
         </div>
-        <div className="series-grid">
-          {tags.map(({ node }) => (
-            <Link to={`/series/${node.slug}`} className="series-card" key={node.id}>
-              {node.feature_image && <div className="series-card-image" style={{ backgroundImage: `url(${node.feature_image})` }}> </div>}
-              <div className="series-card-info">
-                <h2 className="series-card-title">{node.name.replace(`#`, ``)}</h2>
-                <p className="series-card-description">{node.description}</p>
-                <span className="series-card-count">{node.postCount} Posts</span>
-              </div>
-            </Link>
-          ))}
+        <div className="courses">
+          <h2 className="course-section-title">Data Science & Engineering</h2>
+          <div className="series-grid">
+            {dataScienceCourses.map(({ node }) => (
+              <CoursePreview course={node} key={node.id} />
+            ))}
+          </div>
+          <h2 className="course-section-title">Software Engineering</h2>
+          <div className="series-grid">
+            {softwareCourses.map(({ node }) => (
+              <CoursePreview course={node} key={node.id} />
+            ))}
+          </div>
+          <h2 className="course-section-title">Data Analysis</h2>
+          <div className="series-grid">
+            {analysisCourses.map(({ node }) => (
+              <CoursePreview course={node} key={node.id} />
+            ))}
+          </div>
+          <h2 className="course-section-title">Cloud Architecture</h2>
+          <div className="series-grid">
+            {cloudCourses.map(({ node }) => (
+              <CoursePreview course={node} key={node.id} />
+            ))}
+          </div>
         </div>
       </Layout>
     </>
@@ -54,7 +73,46 @@ const SeriesArchive = ({ data, location, pageContext }) => {
 
 SeriesArchive.propTypes = {
   data: PropTypes.shape({
-    allGhostTag: PropTypes.object.isRequired,
+    datascience: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        postCount: PropTypes.number.isRequired,
+        feature_image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    software: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        postCount: PropTypes.number.isRequired,
+        feature_image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    cloud: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        postCount: PropTypes.number.isRequired,
+        feature_image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    analysis: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        postCount: PropTypes.number.isRequired,
+        feature_image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      })
+    ).isRequired,
     ghostPage: PropTypes.object.isRequired,
   }).isRequired,
   pageContext: PropTypes.shape({
@@ -69,17 +127,53 @@ export default SeriesArchive
 
 export const seriesQuery = graphql`
     query GhostSeriesArchiveQuery($slug: String) {
-        allGhostTag(sort: {order: DESC, fields: postCount}, filter: {visibility: {eq: "internal"}, postCount: {gt: 1}}) {
+        datascience: allGhostTag(sort: {order: DESC, fields: postCount}, filter: {visibility: {eq: "internal"}, postCount: {gt: 1}, slug: {in: ["data-analysis-pandas", "code-snippet-corner", "mapping-data-with-mapbox", "learning-apache-spark", "welcome-to-sql"]}}) {
           edges {
-             node {
-               id
-               slug
-               postCount
-               feature_image
-               description
-               name
-             }
-           }
+            node {
+              id
+              slug
+              postCount
+              feature_image
+              description
+              name
+            }
+          }
+        }
+        software: allGhostTag(sort: {order: DESC, fields: postCount}, filter: {visibility: {eq: "internal"}, postCount: {gt: 1}, slug: {in: ["building-flask-apps", "starting-django", "mastering-sqlalchemy", "graphql-hype", "mongodb-cloud"]}}) {
+          edges {
+            node {
+              id
+              slug
+              postCount
+              feature_image
+              description
+              name
+            }
+          }
+        }
+        cloud: allGhostTag(sort: {order: DESC, fields: postCount}, filter: {visibility: {eq: "internal"}, postCount: {gt: 1}, slug: {in: ["the-rise-of-google-cloud", "create-an-aws-api"]}}) {
+          edges {
+            node {
+              id
+              slug
+              postCount
+              feature_image
+              description
+              name
+            }
+          }
+        }
+        analysis: allGhostTag(sort: {order: DESC, fields: postCount}, filter: {visibility: {eq: "internal"}, postCount: {gt: 1}, slug: {in: ["adventures-in-excel", "microsoft-powerpivot", "hacking-tableau-server"]}}) {
+          edges {
+            node {
+              id
+              slug
+              postCount
+              feature_image
+              description
+              name
+            }
+          }
         }
         courses: allGhostPost(filter: {tags: {elemMatch: {visibility: {eq: "internal"}}}}) {
           group(field: primary_tag___slug) {
