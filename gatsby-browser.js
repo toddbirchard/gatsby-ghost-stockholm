bbb2abe703b7a351d57740dcb588ff0b70d76c85
Todd Birchard <toddbirchard@gmail.com>
2020-06-26 08:31:22 -0400

    Client-rendered logic refactor.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,33 +4,42 @@ import * as basicLightbox from 'basiclightbox';
 import 'lazysizes';
 import './src/styles/app.less';
 
-
 /*
  * NOTICE: ES6 module exports are not officially supported because of NodeJs
  * https://github.com/gatsbyjs/gatsby/pull/9239
  *
  * ES6 modules are here used because PrismJS should not work with CommonJs.
  */
-  Prism.plugins.NormalizeWhitespace.setDefaults({'remove-trailing': true, 'remove-indent': true, 'left-trim': true, 'right-trim': true});
+
+let path = location.pathname;
+
+// Determine if post
+if ((path.split('/').length - 1) === 2) {
+  // Code Syntax Highlighting
+  Prism.plugins.NormalizeWhitespace.setDefaults({
+    'remove-trailing': true,
+    'remove-indent': true,
+    'left-trim': true,
+    'right-trim': true
+  });
   Prism.highlightAll();
-  let path = location.pathname;
-  if ((path.split('/').length - 1) === 2) {
-    let images = document.querySelectorAll('.kg-image-card img');
-    if (images.length > 0) {
-      for (let image in images) {
-        if (image < images.length) {
-          images[image].onclick = () => {
-            let html = `<img src="` + images[image].getAttribute('src') + `" />`;
-            const instance = basicLightbox.create(html, {
-              onShow: (instance) => {
-          instance.element().style.opacity = 1
-              },
-              onClose: (instance) => {
-                instance.element().style.opacity = 0
-              }
-            }).show()
-          }
+  // Enable lightbox on images
+  let images = document.querySelectorAll('.kg-image-card img');
+  if (images.length > 0) {
+    for (let image in images) {
+      if (image < images.length) {
+        images[image].onclick = () => {
+          let html = `<img src="` + images[image].getAttribute('src') + `" />`;
+          const instance = basicLightbox.create(html, {
+            onShow: (instance) => {
+              instance.element().style.opacity = 1
+            },
+            onClose: (instance) => {
+              instance.element().style.opacity = 0
+            }
+          }).show()
         }
       }
     }
   }
+}
