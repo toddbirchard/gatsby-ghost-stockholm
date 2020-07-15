ae425ab2597b35f7ffed2fef2c1b17e6e1ace5f9
Todd Birchard <toddbirchard@gmail.com>
2020-07-15 00:36:15 -0400

    Refactor Gatsby Browser logic.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -15,29 +15,72 @@ export const onRouteUpdate = ({location}) => {
 
   let path = location.pathname;
   if ((path.split('/').length - 1) === 2) {
+    // Posts
+    codeSyntaxHighlight();  // Code Syntax Highlighting
+    enableLightboxImages();  // Enable lightbox on post images
+  } else if (path.indexOf('author')) {
+    scrapeUrlMetadata();  // Author website widget
+  }
+}
 
-    // Code Syntax Highlighting
-    Prism.plugins.NormalizeWhitespace.setDefaults({'remove-trailing': true, 'remove-indent': true, 'left-trim': true, 'right-trim': true});
-    Prism.highlightAll();
+// Posts
+// -------------------------------------------
 
-    // Enable lightbox on images
-    let images = document.querySelectorAll('.kg-image-card img');
-    if (images.length > 0) {
-      for (let image in images) {
-        if (image < images.length) {
-          images[image].onclick = () => {
-            let html = `<img src="` + images[image].getAttribute('src') + `" alt="` + images[image].getAttribute('alt') + `" />`;
-            const instance = basicLightbox.create(html, {
-              onShow: (instance) => {
-                instance.element().style.opacity = 1
-              },
-              onClose: (instance) => {
-                instance.element().style.opacity = 0
-              }
-            }).show()
-          }
+function codeSyntaxHighlight() {
+  Prism.plugins.NormalizeWhitespace.setDefaults({
+    'remove-trailing': true,
+    'remove-indent': true,
+    'left-trim': true,
+    'right-trim': true
+  });
+  Prism.highlightAll();
+}
+
+function enableLightboxImages() {
+  let images = document.querySelectorAll('.kg-image-card img');
+  if (images.length > 0) {
+    for (let image in images) {
+      if (image < images.length) {
+        images[image].onclick = () => {
+          let html = `<img src="` + images[image].getAttribute('src') + `" alt="` + images[image].getAttribute('alt') + `" />`;
+          const instance = basicLightbox.create(html, {
+            onShow: (instance) => {
+              instance.element().style.opacity = 1
+            },
+            onClose: (instance) => {
+              instance.element().style.opacity = 0
+            }
+          }).show()
         }
       }
     }
+  }
+}
+
+
+// Authors
+// -------------------------------------------
+
+function scrapeUrlMetadata() {
+  console.log('on author page');
+  let url = document.getElementById('author-website').getAttribute('href');
+  if (url) {
+    let client = new HttpClient();
+    let endpoint = 'https://hackersandslackers.com/.netlify/functions/scrapemeta?url=' + url;
+    client.get(endpoint, function(response) {
+      console.log('response = ' + response)
+    });
+  }
+}
+
+let HttpClient = function() {
+  this.get = function(url, aCallback) {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200)
+        aCallback(httpRequest.responseText);
+      }
+    httpRequest.open("GET", url, true);
+    httpRequest.send(null);
   }
 }
--- src/components/sidebar/authors/AuthorWebsite.js
+++ src/components/sidebar/authors/AuthorWebsite.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const AuthorWebsite = ({ authorWebsite }) => (
   <div className="widget website">
-    <a href={authorWebsite} className="website-link" target="_blank" rel="noopener noreferrer">{authorWebsite}</a>
+    <a href={authorWebsite} id="author-website" target="_blank" rel="noopener noreferrer">{authorWebsite}</a>
   </div>
 )
 
