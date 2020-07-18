e3830e3ec8f1e2636cbd74c791a5aa09c8f9c597
Todd Birchard <toddbirchard@gmail.com>
2020-07-18 10:23:04 -0400

    Update user lambda.

--- go-src/user/main.go
+++ go-src/user/main.go
@@ -2,34 +2,46 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	accountInfo := GetSessionInfo("https://hackersandslackers.app/members/api/member/")
+	req := CreateRequest(request)
+	userData := GetUserSession(req)
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers:    map[string]string{"Content-Type": "application/json"},
-		Body:       accountInfo,
+		Body:       userData,
 	}, nil
 }
 
-func GetSessionInfo(url string) string {
-	// Create HTTP client with timeout
-	client := &http.Client{
-		Timeout: 30 * time.Second,
+func CreateRequest(request events.APIGatewayProxyRequest) *http.Request {
+	endpoint, err := url.Parse("https://hackersandslackers.app/members/api/member/")
+	var headers = http.Header{}
+	headers.Add("cookie", request.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println(endpoint)
+	fmt.Println(headers)
+	req := &http.Request{URL: endpoint, Header: headers}
+	return req
+}
 
+func GetUserSession(req *http.Request) string {
 	// Request account information by session token.
-	res, resError := client.Get(url)
-	if resError != nil {
-		log.Fatal(resError)
+	client := Client()
+	res, reqError := client.Do(req)
+	if reqError != nil {
+		log.Fatal(reqError)
 	}
 	if res.StatusCode != 200 {
 		log.Fatal("status code error: %i", res.StatusCode)
@@ -41,11 +53,18 @@ func GetSessionInfo(url string) string {
 	if bodyErr != nil {
 		log.Fatal(bodyErr)
 	}
-
 	return string(data)
 }
 
+func Client() *http.Client {
+	// Create HTTP client
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+	return client
+}
+
 func main() {
-	// Make the Handler available
+	// Initialize Handler
 	lambda.Start(Handler)
 }
