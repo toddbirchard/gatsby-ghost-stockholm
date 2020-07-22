88826b6ddc8ec04949d7a86747f2846e14b35ca4
Todd Birchard <toddbirchard@gmail.com>
2020-07-22 14:29:32 -0400

    Update dependencies.

--- src/templates/page.js
+++ src/templates/page.js
@@ -4,7 +4,6 @@ import { graphql } from 'gatsby'
 import { Layout } from '../components/common'
 import { AuthorList } from '../components/authors'
 import { MetaData } from '../components/common/meta'
-
 import '../styles/pages/page.less'
 
 /**
