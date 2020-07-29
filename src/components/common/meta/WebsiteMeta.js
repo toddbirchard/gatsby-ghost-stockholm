e07b69a1512873608bea7329a1c44380b1ac0fb5
Todd Birchard <toddbirchard@gmail.com>
2020-07-29 03:07:15 -0400

    Preload fonts.

--- src/components/common/meta/WebsiteMeta.js
+++ src/components/common/meta/WebsiteMeta.js
@@ -45,7 +45,7 @@ const WebsiteMeta = ({ data, settings, canonical, title, description, image, pag
         {settings.twitter ? <meta name="twitter:site" content={settings.twitter} /> : null}
         {config.creator.twitter ? <meta name="twitter:creator" content={config.creator.twitter} /> : null}
         {googleVerificationID ? <meta name="google-site-verification" content={googleVerificationID} /> : null}
-        <link href="/css/fonts.css" rel="preload" as="style" media="all" />
+        <link href="/css/fonts.css" rel="stylesheet" />
         <script type="application/ld+json">{`
               {
                   "@context": "https://schema.org/",
