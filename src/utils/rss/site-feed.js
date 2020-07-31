5751b26cfff3e6496ae2e575982867f98d0df8ff
Todd Birchard <toddbirchard@gmail.com>
2020-07-31 16:37:46 -0400

    Cleanup minor bugs.

--- src/utils/rss/site-feed.js
+++ src/utils/rss/site-feed.js
@@ -6,47 +6,36 @@ const matchPattern = undefined
 const feedUrl = `/rss.xml`
 const externalLink = `https://feedly.com/i/subscription/feed/https://hackersandslackers.com/rss.xml`
 const feedRssQuery = `{
-    allGhostPost(sort: {order: DESC, fields: published_at}, filter: primary_author: {slug: {eq: "todd"}}}) {
-        edges {
-            node {
-                # Main fields
-                id
-                title
-                slug
-                featured
-                feature_image
-
-                # Dates unformatted
-                created_at
-                published_at
-                updated_at
-
-                # SEO
-                excerpt
-                meta_title
-                meta_description
-
-                # Authors
-                authors {
-                    name
-                }
-                primary_author {
-                    name
-                }
-                tags {
-                    name
-                    visibility
-                }
-
-                # Content
-                html
-
-                # Additional fields
-                url
-                canonical_url
-            }
+  allGhostPost(sort: {order: DESC, fields: published_at}, filter: {primary_author: {slug: {eq: "todd"}}}) {
+    edges {
+      node {
+        id
+        title
+        slug
+        featured
+        feature_image
+        created_at
+        published_at
+        updated_at
+        excerpt
+        meta_title
+        meta_description
+        authors {
+          name
         }
+        primary_author {
+          name
+        }
+        tags {
+          name
+          visibility
+        }
+        html
+        url
+        canonical_url
+      }
     }
+  }
 }`
 
 const siteRssFeed = generateRssFeed(siteConfig, feedRssQuery, feedTitle, feedUrl, matchPattern, externalLink)
