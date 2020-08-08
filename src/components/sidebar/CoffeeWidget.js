b331d46251b289c657c20895feca978ed7ff19d4
Todd Birchard <toddbirchard@gmail.com>
2020-08-08 00:01:30 -0400

    BuyMeACoffee widget.

--- src/components/sidebar/CoffeeWidget.js
+++ src/components/sidebar/CoffeeWidget.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import config from '../../utils/siteConfig'
 import { StaticQuery, graphql } from 'gatsby'
+import { FiCoffee } from 'react-icons/fi'
 
 /**
 * BuyMeACoffee widget
@@ -9,28 +10,33 @@ import { StaticQuery, graphql } from 'gatsby'
 
 const CoffeeWidget = ({ data }) => {
   const coffeeImg = `/images/bmc-new-btn-logo.svg`
-  const donateCopy = `We started sharing these tutorials to help and inspire new scientists and engineers around the world. If Hackers and Slackers has been helpful to you, feel free to buy us a coffee to keep us going :).`
-  const donations = data.coffees.items.edges
+  const donateCopy = `If Hackers and Slackers has been helpful to you, feel free to buy us a coffee to keep us going :).`
+  const donations = data.coffees.edges
 
   return (
     <>
       <div className="coffee widget">
-        <h4 className="widget-title">Support us</h4>
+        <h4 className="widget-title">Top Contributors</h4>
         <p className="widget-description">{donateCopy}</p>
+        <div className="donations">
+          {donations.map(({ node }) => (
+            <div key={node.id} className="donation">
+              <div className="contributor">
+                <div className="name">{node.name}</div>
+                <p className="message">{node.message}</p>
+              </div>
+              <div className="contributed">
+                <div>
+                  <span className="count">{node.count}</span>
+                  <FiCoffee />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
         <a rel="noopener noreferrer" target="_blank" href={config.links.buyMeACoffee}>
           <button className="widget-button" >
             <span className="button-content">Buy me a coffee</span>
-            {donations.map(({ node }) => (
-              <div key={node.id}>
-                <div>
-                  <span>{node.name}</span>
-                  <p>{node.description}</p>
-                </div>
-                <div>
-                  <span>{node.count}</span>
-                </div>
-              </div>
-            ))}
             <img src={coffeeImg} className="coffee-icon" alt="Buy us a coffee" />
           </button>
         </a>
@@ -41,16 +47,18 @@ const CoffeeWidget = ({ data }) => {
 
 CoffeeWidget.propTypes = {
   data: PropTypes.shape({
-    coffees: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        count: PropTypes.number.isRequired,
-        message: PropTypes.string,
-        email: PropTypes.string,
-        link: PropTypes.string.isRequired,
-      }),
-    ),
+    coffees: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          name: PropTypes.string.isRequired,
+          count: PropTypes.number.isRequired,
+          message: PropTypes.string,
+          email: PropTypes.string,
+          link: PropTypes.string.isRequired,
+        }),
+      ),
+    }),
   }),
 }
 
@@ -58,13 +66,17 @@ const CoffeeWidgetQuery = props => (
   <StaticQuery
     query={graphql`
       query coffeeQuery {
-        coffees: mysqlDonations {
-          id
-          count
-          name
-          message
-          link
-          email
+        coffees: allMysqlDonations(limit: 3, sort: {fields: count, order: DESC}) {
+          edges {
+            node {
+              email
+              id
+              count
+              link
+              message
+              name
+            }
+          }
         }
       }`
     }
