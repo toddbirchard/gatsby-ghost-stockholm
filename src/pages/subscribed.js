b704848044a10b7eded27b5c4848fc43f8f9a3e1
Todd <todd.birchard@fanduel.com>
2020-09-14 19:29:20 -0400

    Update dependencies.

--- src/pages/subscribed.js
+++ src/pages/subscribed.js
@@ -4,7 +4,7 @@ import { Layout } from '../components/common'
 import '../styles/pages/confirmation.less'
 
 const title = `Subscribed`
-const description = `You're all set to start receiving our newsletter! We try to only send things worth reading, so we won't be bothering you too often.`
+const description = `You're all set to start receiving our newsletter. We try to only send things worth reading, so we won't be bothering you too often.`
 
 const Subscribed = () => (
   <Layout template="page-template" hasSidebar={false}>
