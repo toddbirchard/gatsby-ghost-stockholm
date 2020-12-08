2ad6ba40cb67d5576db4f2411924ab75c810de1f
Todd Birchard <toddbirchard@gmail.com>
2020-12-08 02:05:59 -0500

    Auth button fix.

--- src/components/auth/AuthButton.js
+++ src/components/auth/AuthButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import PropTypes from 'prop-types'
 import IdentityModal, { useIdentityContext } from "react-netlify-identity-widget"
 
@@ -8,6 +8,9 @@ const AuthButton = ({ styleClass }) => {
   const isLoggedIn = identity && identity.isLoggedIn
   const [buttonText, setButtonText] = useState(isLoggedIn ? `Sign out` : `Sign up`)
 
+  useEffect(() => {
+    setButtonText(isLoggedIn ? `Sign out` : `Sign up`)
+  })
   const handleClick = () => {
     if (isLoggedIn) {
       identity.logoutUser()
@@ -15,6 +18,20 @@ const AuthButton = ({ styleClass }) => {
     } else {
       setDialog(true)
     }
+  }
+  const handleLogin = (u) => {
+    console.log(`---user info----`)
+    console.log(u.id)
+    console.log(u.user_metadata.full_name)
+    console.log(u.user_metadata.user_avatar)
+    console.log(u.app_metadata.provider)
+    console.log(u.email)
+    console.log(`--- values ----`)
+    console.log(`isLoggedIn = ` + isLoggedIn)
+  }
+  const handleLogout = () => {
+    console.log(`--- values ----`)
+    console.log(`isLoggedIn = ` + isLoggedIn)
   }
   return (
     <>
@@ -27,7 +44,9 @@ const AuthButton = ({ styleClass }) => {
       <IdentityModal
         showDialog={dialog}
         onCloseDialog={() => setDialog(false)}
-        onLogout={() => setButtonText(`Sign up`)}
+        onLogin={u => handleLogin(u)}
+        onSignup={u => handleLogin(u)}
+        onLogout={() => handleLogout()}
       />
     </>
   )
