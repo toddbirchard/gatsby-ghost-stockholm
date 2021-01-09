a75acfb9bb20be52979de039efd5a6f51e33b770
Todd Birchard <toddbirchard@gmail.com>
2021-01-08 23:23:57 -0500

    Newsletter form update.

--- src/components/sidebar/NewsletterWidget.js
+++ src/components/sidebar/NewsletterWidget.js
@@ -1,59 +1,89 @@
-import React from 'react'
+import React from "react"
 import { FaArrowRight } from 'react-icons/fa'
+import fetch from 'node-fetch'
+import { navigate } from 'gatsby'
 
 /**
  * Newsletter widget
  */
 
-const NewsletterWidget = () => (
-  <>
-    <div className="widget newsletter">
-      <h4 className="widget-title">Newsletter</h4>
-      <form
-        name="newsletter"
-        method="POST"
-        netlify
-        data-netlify="true"
-        netlify-honeypot="phone-number"
-        action="/subscribed/"
-      >
-        <fieldset>
-          <label className="hidden-label" htmlFor="name">Name</label>
-          <input
-            className="subscribe-input-class"
-            type="name"
-            name="name"
-            placeholder="Your name"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label className="hidden-label" htmlFor="email">Email</label>
-          <input
-            className="subscribe-input-class"
-            type="email"
-            name="email"
-            placeholder="Your email address"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <label className="hidden-label" htmlFor="phone-number">Phone</label>
-          <input
-            className="hidden-label"
-            type="phone"
-            name="phone-number"
-            placeholder="Your phone number"
-            style={{ visibility: `hidden` }}
-            autoComplete="off"
-          />
-        </fieldset>
-        <button type="submit" className="newsletter-button">
-          <span>Sign Up</span> <FaArrowRight />
-        </button>
-      </form>
-    </div>
-  </>
-)
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + `=` + encodeURIComponent(data[key]))
+    .join(`&`)
+}
+
+const NewsletterWidget = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    console.log(`form.elements = ` + form.elements)
+    //console.log(`name = ` + form.getElementById(`newsletter-email-field`).value)
+    fetch(`/`, {
+      method: `POST`,
+      headers: { 'Content-Type': `application/x-www-form-urlencoded` },
+      body: encode({
+        'form-name': form.getAttribute(`name`),
+        name: form.elements.namedItem(`name`).value,
+        email: form.elements.namedItem(`email`).value,
+      }),
+    })
+      .then(() => navigate(`/subscribed/`))
+      .catch(error => console.log(error))
+  }
+
+  return (
+    <>
+      <div className="widget newsletter">
+        <h4 className="widget-title">Newsletter</h4>
+        <form
+          name="newsletter"
+          method="POST"
+          netlify
+          data-netlify="true"
+          netlify-honeypot="phone-number"
+          onSubmit={handleSubmit}
+        >
+          <fieldset>
+            <label className="hidden-label" htmlFor="name">Name</label>
+            <input
+              id="newsletter-name-field"
+              className="subscribe-input-class"
+              type="text"
+              name="name"
+              placeholder="Your name"
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <label className="hidden-label" htmlFor="email">Email</label>
+            <input
+              id="newsletter-email-field"
+              className="subscribe-input-class"
+              type="text"
+              name="email"
+              placeholder="Your email address"
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <label className="hidden-label" htmlFor="phone-number">Phone</label>
+            <input
+              className="hidden-label"
+              type="phone"
+              name="phone-number"
+              placeholder="Your phone number"
+              style={{ visibility: `hidden` }}
+              autoComplete="off"
+            />
+          </fieldset>
+          <button type="submit" className="newsletter-button">
+            <span>Sign Up</span> <FaArrowRight />
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
 
 export default NewsletterWidget
--- src/components/sidebar/authors/AuthorWebsite.js
+++ src/components/sidebar/authors/AuthorWebsite.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types'
 
 const AuthorWebsite = ({ authorWebsite }) => (
   <div className="widget website">
-    <a href={authorWebsite} id="author-website" target="_blank" rel="noopener noreferrer">{authorWebsite}</a>
+    <a
+      href={authorWebsite}
+      id="author-website"
+      target="_blank"
+      rel="noopener noreferrer">
+      {authorWebsite}
+    </a>
   </div>
 )
 
