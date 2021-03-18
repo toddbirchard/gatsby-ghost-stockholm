9c0e216aa9fd9b081676ec5a04eb411417b46b90
Todd <toddbirchard@gmail.com>
2021-03-18 00:42:14 -0400

    JS function.

--- functions/scrape/index.js
+++ functions/scrape/index.js
@@ -3,15 +3,17 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 exports.handler = async function(event, context) {
-  console.log(event)
-  console.log(event["queryStringParameters"]["url"])
   const url = event["queryStringParameters"]["url"]
   if (typeof url === 'undefined') { return false }
+  console.log("url = " + url)
+
 
   (async () => {
 	   try {
     	  const response = await got(url);
+        console.log("response.body = " + response.body)
         const dom = new JSDOM(response.body);
+        console.log("dom = " + dom)
         const nodeList = [...dom.window.document.querySelectorAll('meta')];
     	  console.log("response.body = " + response.body);
         console.log("nodeList = " + nodeList);
