311537d1b95a889abcf5b439178eed4f0717c232
Todd Birchard <toddbirchard@gmail.com>
2021-04-03 17:21:07 -0400

    Prevent BS comment submissions

--- src/components/comments/CommentForm.js
+++ src/components/comments/CommentForm.js
@@ -76,7 +76,12 @@ const CommentForm = ({ post }) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     const form = e.target
-    if (isLoggedIn && value !== `Have something to say?` && value !== `` && value !== undefined) {
+    if (isLoggedIn === false) {
+      console.log('User is not logged in.')
+    }
+    if (value === `Have something to say?` || value === `` || value === undefined) {
+      console.log('Comment body is empty.')
+    }
       fetch(`/`, {
         method: `POST`,
         headers: { 'Content-Type': `application/x-www-form-urlencoded` },
