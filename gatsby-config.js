f6a45bcd84ce872b4906b0637d7881610ca115a0
Todd <toddbirchard@gmail.com>
2021-04-04 16:06:40 -0400

    Algolia.

--- gatsby-config.js
+++ gatsby-config.js
@@ -373,8 +373,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-algolia`,
       options: {
-        apiKey: process.env.ALGOLIA_API_KEY,
         appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
         indexName: process.env.ALGOLIA_INDEX_NAME,
         queries,
         chunkSize: 500, // default: 1000
