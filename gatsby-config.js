3a680220a19ebb77f534bfbd7dfb562675f55cff
Todd <toddbirchard@gmail.com>
2021-04-05 02:35:59 -0400

    Make Agolia variables visible.

--- gatsby-config.js
+++ gatsby-config.js
@@ -336,9 +336,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-algolia`,
       options: {
-        appId: process.env.ALGOLIA_APP_ID,
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.ALGOLIA_INDEX_NAME,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
         matchFields: [`slug`, `updated_at`],
         queries,
         chunkSize: 500, // default: 1000
