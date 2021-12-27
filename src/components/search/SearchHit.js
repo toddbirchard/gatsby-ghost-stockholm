8b4115df188924a5acef9c5a6087f3a1a9ede010
Todd Birchard <toddbirchard@gmail.com>
2021-12-27 15:54:08 -0500

    Resolve mobile search result images.

--- src/components/search/SearchHit.js
+++ src/components/search/SearchHit.js
@@ -4,9 +4,12 @@ import { Link } from 'gatsby'
 
 const SearchHit = ({ hit }) => {
   const featureImage = hit.feature_image
+  const title = hit.title
+  const slug = hit.slug
+  const primary_tag = hit.primary_tag
   const featureImageRetina = featureImage.replaceAll(`.jpg`, `@2x.jpg`).replaceAll(`.png`, `@2x.png`)
   const featureImageSlash = featureImage && featureImage.lastIndexOf(`/`)
-  const featureMobileImage =
+  const featureImageSmall =
     featureImageSlash &&
     [
       featureImageRetina.slice(0, featureImageSlash),
@@ -15,29 +18,29 @@ const SearchHit = ({ hit }) => {
     ].join(``)
 
   return (
-    <Link to={`/${hit.slug}/`}>
+    <Link to={`/${slug}/`}>
       <div className="search-result">
         <div className="image-wrapper">
           <img
             className="search-result-image"
-            src={featureMobileImage}
-            alt={hit.title}
-            title={hit.title}
+            src={featureImageSmall}
+            alt={title}
+            title={title}
           />
         </div>
         <div className="search-result-details">
-          <div className="search-result-title">{hit.title}</div>
-          {hit.primary_tag ? (
+          <div className="search-result-title">{title}</div>
+          {primary_tag ? (
             <Link
-              to={`/tag/${hit.primary_tag.slug}/`}
+              to={`/tag/${primary_tag.slug}/`}
               className="primary-tag"
               style={{
-                background: hit.primary_tag.accent_color,
-                border: `1px solid ${hit.primary_tag.accent_color}`,
+                background: primary_tag.accent_color,
+                border: `1px solid ${primary_tag.accent_color}`,
               }}
             >
               {` `}
-              {hit.primary_tag.name}
+              {primary_tag.name}
               {` `}
             </Link>
           ) : null}
